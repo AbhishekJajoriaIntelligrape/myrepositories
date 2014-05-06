9c236a2b08ee07dfa503edb73d0acaf967a7646d
=AbhishekJajoria <=abhishek.jajoria@intelligrape>
2014-05-06 17:53:27 +0530

    This is new controller added

--- LinkSharing/grails-app/conf/DataSource.groovy
+++ LinkSharing/grails-app/conf/DataSource.groovy
@@ -1,9 +1,11 @@
+import org.hibernate.dialect.MySQL5InnoDBDialect
+
 dataSource {
     pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = MySQL5InnoDBDialect
+    username = "root"
+    password = "igdefault"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -18,7 +20,7 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://localhost:3306/link_sharing"
         }
     }
     test {
